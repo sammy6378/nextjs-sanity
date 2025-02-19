f92618cdc625c73fc806e4eb44b9f8fff79d6b72
UpskillKenya <admin@upskillkenya.co.ke>
2025-02-19 22:15:20 +0300

    feat: update schemaTypes to include eventType and userType

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,8 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import { eventType } from "./EventType";
+import { userType } from "./userTypes";
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [
+    eventType,
+    userType
+]
+
