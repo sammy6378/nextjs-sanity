a8f20ac0f08d9b39b2b65b780b51369a6c90938a
sammy6378 <samuel202mwangi@gmail.com>
2025-02-20 08:15:22 +0300

    feat: add @sanity/icons dependency and update event schema with icon and field groups

--- schemaTypes/EventType.ts
+++ schemaTypes/EventType.ts
@@ -1,35 +1,54 @@
 import { defineField,defineType } from "sanity";
-
+import { CalendarIcon } from "@sanity/icons";
 
 export const eventType = defineType({
     name: 'event',
     title: 'Event',
     type: 'document',
+    icon: CalendarIcon,
+    groups:[
+        {name: 'details', title: 'Details'},
+        {name: 'editorial', title: 'Editorial'}
+    ],
+
+
     fields:[
         defineField({
             name: 'name',
-            type: 'string'
+            type: 'string',
+            group: 'details'
         }),
         defineField({
             name: "slug",
-            type: 'slug'
+            type: 'slug',
+            options: {source: 'name'},
+            validation: Rule => Rule.required().error('Slug is required'),
+            group: 'details'
         }),
         defineField({
             name: 'eventType',
-            type: 'string'
+            type: 'string',
+            options:{
+                list: ['in-person', 'virtual'],
+                layout: 'radio'
+            },
+            group: 'editorial'
         }),
         defineField({
             name: 'date',
-            type: "datetime"
+            type: "datetime",
+            group: 'editorial'
         }),
         defineField({
             name: 'venue',
-            type: 'string'
+            type: 'string',
+            group: 'editorial'
         }),
         defineField({
             name: 'details',
             type: 'array',
-            of: [{type: 'block'}]
+            of: [{type: 'block'}],
+            group: 'editorial'
         }),
         defineField({
             name: 'organizer',
