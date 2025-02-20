7474d2411afe76f7e939f32f2c12bcbdabc4cb4b
sammy6378 <samuel202mwangi@gmail.com>
2025-02-20 09:55:59 +0300

    feat: add live data fetching functionality with next-sanity integration

---
+++ client/src/sanity/live.ts
@@ -0,0 +1,7 @@
+import { defineLive } from "next-sanity";
+import { client } from "./client";
+
+
+export const { sanityFetch, SanityLive} = defineLive({
+    client: client.withConfig({apiVersion: "vX"})
+})
